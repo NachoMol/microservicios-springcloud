54423d084757c66784f9b6542e1ae093d0033f75
NachoMol <ignaciotmolina@gmail.com>
2024-03-05 22:05:10 -0300

    creando model de checkout

---
+++ modulo3/checkout-service/src/main/java/com/digitalhouse/checkoutservice/controller/CheckoutRestController.java
@@ -0,0 +1,13 @@
+package com.digitalhouse.checkoutservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/checkout")
+public class CheckoutRestController {
+    private ICheckoutService checkoutService;
+
+    public CheckoutRestController(IChe)
+
+}
