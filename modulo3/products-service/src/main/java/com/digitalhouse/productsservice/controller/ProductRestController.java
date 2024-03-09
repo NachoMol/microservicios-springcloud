cdfc79d2bea5f85ba8dc400f1ba632fd7519adb8
NachoMol <ignaciotmolina@gmail.com>
2024-03-09 20:18:36 -0300

    implementando url en feign

--- modulo3/products-service/src/main/java/com/digitalhouse/productsservice/controller/ProductRestController.java
+++ modulo3/products-service/src/main/java/com/digitalhouse/productsservice/controller/ProductRestController.java
@@ -13,7 +13,7 @@ public class ProductRestController {
 
     @GetMapping
     public Product getProduct(@RequestParam String id){
-        return new Product(id,"Notebook",2000.0, "Instance 2");
+        return new Product(id,"Notebook",2000.0, "Instance 1");
     }
 
 }
