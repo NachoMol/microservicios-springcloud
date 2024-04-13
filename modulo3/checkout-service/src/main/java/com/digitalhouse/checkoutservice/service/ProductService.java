d0c5f9b5477c34e8d87aec7ea129be8cd781c499
NachoMol <ignaciotmolina@gmail.com>
2024-04-13 12:33:12 -0300

    agregando anotacion para retry

--- modulo3/checkout-service/src/main/java/com/digitalhouse/checkoutservice/service/ProductService.java
+++ modulo3/checkout-service/src/main/java/com/digitalhouse/checkoutservice/service/ProductService.java
@@ -4,6 +4,7 @@ import com.digitalhouse.checkoutservice.dto.Product;
 import com.digitalhouse.checkoutservice.repository.FeignProductRepository;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -24,8 +25,9 @@ public class ProductService implements IProductService{
 
     @Override
     @CircuitBreaker(name="product",fallbackMethod ="getProductFallbackMethod")
+    @Retry(name="product")
     public Product getProduct(String id) {
-
+        log.info("Intentando obtener el producto con ID:" + id);
         return feignProductRepository.getProductById(id,true);
     }
 
